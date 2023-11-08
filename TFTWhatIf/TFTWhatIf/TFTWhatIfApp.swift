21d07e9eae93bc6316f05706b1552ca380ba1aa2
Benny(Gibaek Han) <hkb31415@gmail.com>
2023-11-08 22:00:57 +0900

    [Feat] xcode project 생성

---
+++ TFTWhatIf/TFTWhatIf/TFTWhatIfApp.swift
@@ -0,0 +1,17 @@
+//
+//  TFTWhatIfApp.swift
+//  TFTWhatIf
+//
+//  Created by HAN GIBAEK on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TFTWhatIfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
